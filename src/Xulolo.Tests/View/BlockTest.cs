c6932e64c3911f8dd290d8e60d9102be6e66c71e
tiago.oliveira <tiagoliv28@hotmail.com>
2025-11-14 21:55:21 +0000

    add tests for block view component

---
+++ src/Xulolo.Tests/View/BlockTest.cs
@@ -0,0 +1,44 @@
+﻿using Xulolo.Renderer;
+using Xulolo.View;
+
+namespace Xulolo.Tests.View
+{
+    public class BlockTest
+    {
+        [Fact]
+        public Task BlockWithNoChildrenShouldRenderProperly()
+        {
+            var block = new Block();
+            var renderer = new StringRenderer();
+            block.Render(renderer);
+
+            return Verify(renderer.Content);
+        }
+
+        [Fact]
+        public Task BlockWithSingleChildShouldRenderProperly()
+        {
+            var block = new Block(new Textbox("Sample Text"));
+            var renderer = new StringRenderer();
+
+            block.Render(renderer);
+
+            return Verify(renderer.Content);
+        }
+
+        [Fact]
+        public Task BlockWithMultipleChildrenShouldRenderProperly()
+        {
+            var block = new Block(
+                new Textbox("First Textbox"),
+                new Checkbox("Sample Checkbox", true),
+                new Textbox("Second Textbox")
+            );
+            var renderer = new StringRenderer();
+            
+            block.Render(renderer);
+
+            return Verify(renderer.Content);
+        }
+    }
+}
