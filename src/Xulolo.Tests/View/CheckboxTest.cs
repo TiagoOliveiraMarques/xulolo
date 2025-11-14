a8d86eeac427f4adb6d236d03dbc6f1ca2655fa9
tiago.oliveira <tiagoliv28@hotmail.com>
2025-11-14 21:45:23 +0000

    add tests for checkbox view component

---
+++ src/Xulolo.Tests/View/CheckboxTest.cs
@@ -0,0 +1,23 @@
+﻿using Xulolo.Renderer;
+using Xulolo.View;
+
+namespace Xulolo.Tests.View
+{
+    public class CheckboxTest
+    {
+        [Theory]
+        [InlineData("not checked empty string", "", false)]
+        [InlineData("checked empty string", "", true)]
+        [InlineData("not checked with text", "lorem ipsum", false)]
+        [InlineData("checked with text", "lorem ipsum", true)]
+        public Task CheckboxShouldRenderProperly(string scenario, string text, bool @checked)
+        {
+            var checkbox = new Checkbox(text, @checked);
+            var renderer = new StringRenderer();
+
+            checkbox.Render(renderer);
+
+            return Verify(renderer.Content).UseParameters(scenario);
+        }
+    }
+}
