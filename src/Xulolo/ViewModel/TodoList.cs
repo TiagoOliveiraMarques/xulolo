c90c2ce34dd9117b24679a2f188a9e5ed32b87a2
tiago.oliveira <tiagoliv28@hotmail.com>
2025-11-14 22:05:15 +0000

    cleanup namespaces

--- src/Xulolo/ViewModel/TodoList.cs
+++ src/Xulolo/ViewModel/TodoList.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Xulolo.Model;
-using Xulolo.Renderer;
+﻿using Xulolo.Model;
 using Xulolo.View;
 
 namespace Xulolo.ViewModel
